a8e4417e8a7b8f0bc277edd7a081aecd598bbde3
Jesse Steele <jesselsteele@users.noreply.github.com>
2015-03-26 14:09:06 +0800

    Update cloudaccounts.php

--- cloudaccounts.php
+++ cloudaccounts.php
@@ -28,9 +28,9 @@ function add_toolbar_items($admin_bar){
 	$admin_bar->add_menu( array(
 		'id'    => 'cloudaccountslogin-email',
 		'title' => 'Mail',
-		'href'  => 'https://mail.verb.ink',	
+		'href'  => 'https://verb.email',	
 		'meta'  => array(
-			'title' => __('Webmail login | mail.verb.ink'),
+			'title' => __('Webmail login | verb.email'),
 			'target' => __('_blank'),
 		),
 	));
@@ -39,7 +39,7 @@ function add_toolbar_items($admin_bar){
 		'title' => 'Cloud',
 		'href'  => 'https://verb.blue',	
 		'meta'  => array(
-			'title' => __('Cloud login | cloud.verb.ink'),
+			'title' => __('Cloud login | verb.blue'),
 			'target' => __('_blank'),
 		),
 	));
@@ -128,7 +128,7 @@ ob_start();?>
   
   <p>
   <strong>Login URL: </strong>
-  <a target="_blank" href="https://mail.verb.ink">mail.verb.ink</a>
+  <a target="_blank" href="https://verb.email">verb.email</a>
   <br />
   <em>(Login to use webmail, set email forwarding, and change your password.)</em>
   </p>
@@ -171,10 +171,10 @@ ob_start();?>
   <em>A. set up at least one email client with POP3 to keep the email off the server so your Inbox doesn't get full or</em>
   </p>
   <p>
-  <em>B. login to webmail at <a target="_blank" href="https://mail.verb.ink">mail.verb.ink</a>, go to settings, and set up a forwarding alias email address that your email will go to and uncheck the box so that email does not stay on the server. (This is probably the best option, you can still send mail using SMTP.)</em>
+  <em>B. login to webmail at <a target="_blank" href="https://verb.email">verb.email</a>, go to settings, and set up a forwarding alias email address that your email will go to and uncheck the box so that email does not stay on the server. (This is probably the best option, you can still send mail using SMTP.)</em>
   </p>
   <p>
-  <em>If you have a $2 subscription, then we will allow your Inbox to slowly take more email up to 10GB (not all at once, you must take at least three years to max out or we'll want to know why.) We may increase this even more, several years into the future. With this, you can use IMAP on your email clients, which will sync your email with the server, you can then view all your email on the webserver and any email clients that you set up, and space won't be as big of a problem. Even after 10 years a lots of email, you probably won't even fill up 5GB. If you don't plan to receive email at your verb.ink email address, then the $1 subscription should be fine and you can still use it to send email via SMTP.</em>
+  <em>If you have a $2 InfiniteVerb&trade; subscription, then we will allow your Inbox to slowly take more email up to 10GB (not all at once, you must take at least three years to max out or we'll want to know why.) We may increase this even more, several years into the future. With this, you can use IMAP on your email clients, which will sync your email with the server, you can then view all your email on the webserver and any email clients that you set up, and space won't be as big of a problem. Even after 10 years a lots of email, you probably won't even fill up 5GB. If you don't plan to receive email at your verb.ink email address, then the $1 subscription should be fine and you can still use it to send email via SMTP.</em>
   </p>
   <p>
   <em>As per the conditions, remember that, by using this service, you agree not to spam. You are allowed "normal" bandwidth and system resources for email that a normal or an "above average" productive individual may use. If we discover that you are using this account to send massive amounts of email, as a clear violation of the terms, you could be billed an additional $50 or twice the extra costs, whichever is higher. If you are a normal, cool person who doesn't spam, even though you may be active on the web with business contacts, this shouldn't ever be a problem. For massive email campaigns, we encourage you to look into MailChimp or contact us to purchase a custom plan for large-scale commercial use. We may be very helpful for that if you are up front about your goals. Remember, verb.ink is for single-individual high-productivity use, whether for business, non-profit, or personal.</em>
